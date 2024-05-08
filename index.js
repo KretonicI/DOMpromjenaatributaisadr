a41624c0baab9e2a2efb86b9f7e081b9824cbfa5
office10 <office10@ucione.local>
2024-05-08 18:28:53 +0200

    dodavanje vježbe

---
+++ index.js
@@ -0,0 +1,26 @@
+var rijeke =["Mrežnica", "Dobra", "Korana", "Kupa", "Karlovačko"];
+var h1 = document.createElement("h1");
+h1.appendChild(document.createTextNode ("Karlovac"));
+
+var bodyElem = document.querySelector("body");
+bodyElem.prepend(h1);
+
+var dDescr = document.querySelector(".description");
+dDescr.innerHTML = "Grad na <h3> 5 rijeka </h3>"
+
+var divInfo = document.createElement("div");
+divInfo.classList.add("info");
+divInfo.innerHTML = "Peta rijeka je Piva.";
+var footElement = document.querySelector("#footer");
+footElement.parentNode.insertBefore(divInfo,footElement);
+
+var ul = document.querySelector("ul");
+for (let i = ul.childElementCount; i > 0; i--) {
+    let child = ul.children[ i - 1];
+    ul.removeChild(child);
+}
+
+for (let i = 0; i < 5; i++) {
+    var li = document.createElement("li");
+    li.textContent = rijeke [i];
+    ul.appendChild(li);}
